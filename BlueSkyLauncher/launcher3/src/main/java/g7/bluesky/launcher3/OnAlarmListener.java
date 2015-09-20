c4f5ba8cc6ca7f890deda161668653f191f95533
Phạm Tuấn <tuanpt216@gmail.com>
2015-09-21 06:12:56 +0700

    Rename package, Android App, remove LauncherBackupHelper.java to avoid compile error, move package launcher3 from WallpaperPicker module to Launcher3 module (fixed R class not found)

---
+++ BlueSkyLauncher/launcher3/src/main/java/g7/bluesky/launcher3/OnAlarmListener.java
@@ -0,0 +1,5 @@
+package g7.bluesky.launcher3;
+
+public interface OnAlarmListener {
+    public void onAlarm(Alarm alarm);
+}
---
+++ BlueSkyLauncher/launcher3/src/main/java/g7/bluesky/launcher3/ToggleWeightWatcher.java
@@ -0,0 +1,7 @@
+package g7.bluesky.launcher3;
+
+import android.app.Activity;
+
+public class ToggleWeightWatcher extends Activity {
+
+}
